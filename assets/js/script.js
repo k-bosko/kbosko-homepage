839d6f25559fb420d11d391fa8b857bf43da2fef
Katerina Bosko <katerinabosko@gmail.com>
2022-09-23 16:43:14 -0700

    added footer and back-to-top scroller

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,12 +24,51 @@ document.addEventListener("DOMContentLoaded", () => {
     // either html or text
     contentType: "html",
     // call when done callback function
-    callback: function () {},
+    callback: function () { },
     // starting callback function before each string
-    preStringTyped: function () {},
+    preStringTyped: function () { },
     //callback for every typed string
-    onStringTyped: function () {},
+    onStringTyped: function () { },
     // callback for reset
-    resetCallback: function () {},
+    resetCallback: function () { },
   });
 });
+
+
+(function () {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+  /**
+     * Back to top button
+     */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+})()
